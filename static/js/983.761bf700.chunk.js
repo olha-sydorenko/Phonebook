dd7501207382aab72b41409af139377235fc1520
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-03-04 20:57:02 +0000

    Deploying to gh-pages from @ olha-sydorenko/goit-react-hw-08-phonebook@0e7ad1d8fa30c766b9446d7e2bff5c1659988f2c 🚀

---
+++ static/js/983.761bf700.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});n(2791);var r=n(184);function o(){return(0,r.jsx)("h1",{children:"Phonebook"})}}}]);
+//# sourceMappingURL=983.761bf700.chunk.js.map
